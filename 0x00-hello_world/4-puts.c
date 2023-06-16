f54243158d83810e37113731a3dd4e53feaf6779
sadam <paulmuthama60@gmail.com>
2023-06-16 00:49:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main
+ * main: entry of the code
  *
- * prints line in quotation
+ *description: prints line in quotation
  *
  * return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
