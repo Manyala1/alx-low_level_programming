d0047d169a4ecf53087fbf1497aa1e3050666daa
sadam <paulmuthama60@gmail.com>
2023-06-16 11:20:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry of the program
+ *
+ * program prints the last part of the line, followed by new line after the standard error
+ *
+ * rturn: 1 when succcess
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
