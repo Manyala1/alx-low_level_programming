a88c7635b4a5b53d7851f54659d4430fdf4cf1d6
sadam <paulmuthama60@gmail.com>
2023-06-16 00:40:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * prints line in quotation
  *
- * return 0
+ * return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n");
+	puts(" "Programming is like building a multilingual puzzle\n" ");
 	return (0);
 }
