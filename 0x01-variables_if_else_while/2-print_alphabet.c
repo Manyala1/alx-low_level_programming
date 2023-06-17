1caa63267d727d197ebfd8422cb88109b5e5ba55
sadam <paulmuthama60@gmail.com>
2023-06-17 07:59:53 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,25 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: print value of n satus; zero, positive or negative
+ * Description: print all aplhabet letters
  *
  * Return: Always 0 (Success)
 */
-int myFunction(void){
-	return (0);
-}
+
 int main(void)
 {
-	int n = (int)myFunction; 
+	char ch = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
 
 	return (0);
 }
+
