63c5c25634f1d51f227b42e861fecc00eefc28fb
sadam <paulmuthama60@gmail.com>
2023-06-20 02:39:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * prints line in quotation
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
