ec7b1cb51291611f33b8c46ce43e1179d5ffac46
sadam <paulmuthama60@gmail.com>
2023-06-21 08:10:28 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,12 @@ int print_last_digit(int i)
         int j;
 
         if (i > 0)
-                i = -i;
+        	i = -i;
 
         j = i % 10;
 
-        if (j < 0)
-                j = -j;
+        if (j  > 0)
+		j = -j;
 
         _putchar(j + '0');
 
