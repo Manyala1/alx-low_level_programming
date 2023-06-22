a36df4f8b529362adf5fb00c4df662f2afc8ab36
sadam <paulmuthama60@gmail.com>
2023-06-22 04:15:46 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**main - test positivty or neativity of a function
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
