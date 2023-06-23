e27eedf139d23975b656a5c1922bb7c66a8d1074
sadam <paulmuthama60@gmail.com>
2023-06-23 13:52:20 -0700

    ./101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,14 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
-
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -1;
+		_putchar('-');
+		n = -n;
 	}
-	if (i / 10)
+	if (n / 10)
 	{
-		print_number(i / 10);
+		print_number(n / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar('0' + (n % 10));
 }
