2142515eb557a6e177c8bc954ecfff39fd171eb2
sadam <paulmuthama60@gmail.com>
2023-06-23 14:14:16 -0700

    ./5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,23 +3,19 @@
 
 /**
  * more_numbers - prints 10 times the numbers from 0 to 14
- *
- * Return: void
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
-			_putchar('1');
-
-			_putchar(j % 10 + '0');
+			if (i >= 10)
+				_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
 		}
-
 		_putchar('\n');
 	}
 }
