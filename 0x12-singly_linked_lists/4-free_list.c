8d8348af370c88f15331d047bff3b166769e82cb
Paul <paulmuthama60@gmail.com>
2023-10-20 13:40:45 -0700

    ./e

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - releases the memomry allocated for a list
+ * @head: pointer to the first node of the list
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
